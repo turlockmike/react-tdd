44efa5a893e5b2fa9a4b3d3c1986a98a12860c7e
Michael Darmousseh <michael.darmousseh@docusign.com>
2018-01-08 23:37:39 -0800

    base starting place

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,11 +1,12 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 
 import App from "./App";
 
-it("renders correctly Right", () => {
-  const element = shallow(<App />);
-  expect(element.text()).toEqual("Clicks: 0")
-  element.find('button').simulate('click')
-  expect(element.text()).toEqual("Clicks: 1")
-});
+
+describe('App', () => {
+  it('renders null', () => {
+    const element = shallow(<App />)
+    expect(element.html()).toEqual(null)
+  });
+})
