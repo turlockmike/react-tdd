4cb0deb5c03497255f3da882977694eacf5511a1
Michael Darmousseh <michael.darmousseh@docusign.com>
2018-02-05 20:13:33 -0800

    better version

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'React';
-
-class App extends React.Component {
-  render() {
-
-  }
-}
-
-export default App;
---
+++ src/Calculator.test.js
@@ -0,0 +1,4 @@
+import React from 'react'
+import Calculator from "./Calculator";
+import { shallow } from 'enzyme'
+import Faker from 'faker'
