eab52612cd93a542a03debedd4f3a35c4fa65e30
Michael Darmousseh <michael.darmousseh@docusign.com>
2018-02-05 22:51:58 -0800

    added requirements

--- src/Calculator.test.js
+++ src/Calculator.test.js
@@ -2,3 +2,9 @@ import React from 'react'
 import Calculator from "./Calculator";
 import { shallow } from 'enzyme'
 import Faker from 'faker'
+
+//It shows a running total that starts at 0
+//Allows a user to input a number
+//When a user clicks 'add' it adds it to the running total
+//It doesn't allow non numerical characters (except '-' for minus sign)
+//It automatically clears the field after clicking add
